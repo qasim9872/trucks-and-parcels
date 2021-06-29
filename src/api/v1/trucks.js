6e6b73fa89871908c07b5a2b8a7f0430ed6e590a
Muhammad Qasim <muhammad.qasim@accenture.com>
2021-06-29 10:54:16 +0100

    added placeholders

--- src/api/v1/trucks.js
+++ src/api/v1/trucks.js
@@ -1,7 +1,32 @@
 const Router = require('express-promise-router');
+const { trucks } = require('../../database');
 
 const router = new Router();
 
-router.get('/', (req, res) => res.status(200).send('Trucks - OK'));
+router.get('/', (req, res) => {
+  res.status(200).send('Trucks - Get All Trucks');
+});
+
+router.post('/', (req, res) => {
+  res.status(201).send('Trucks - Create new Truck');
+});
+
+router.get('/:id', (req, res) => {
+  res
+    .status(200)
+    .send(`Trucks - Get Truck with id: ${req.params.id}`);
+});
+
+router.delete('/:id', (req, res) => {
+  res
+    .status(200)
+    .send(`Trucks - Delete Truck with id: ${req.params.id}`);
+});
+
+router.put('/:id', (req, res) => {
+  res
+    .status(200)
+    .send(`Trucks - Update Truck with id: ${req.params.id}`);
+});
 
 module.exports = router;
---
+++ src/database/index.js
@@ -0,0 +1,3 @@
+const trucks = [];
+
+module.exports = { trucks };
